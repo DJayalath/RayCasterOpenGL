f4282170a5adcc07f552d1fa37fd7e6d2e1bec9a
Dulhan Jayalath <dhjayalath@gmail.com>
2019-06-21 17:27:19 +0100

    Untextured operational

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 #include <string>
 #include <iostream>
@@ -126,27 +128,6 @@ int main(int argc, char* argv[])
 	double time = 0; //time of current frame
 	double oldTime = 0; //time of previous frame
 
-	UINT32 buffer[W_WIDTH * W_HEIGHT];
-	UINT32 texture[NUM_TEXTURES * TEX_WIDTH * TEX_HEIGHT];
-
-	//generate some textures
-	for (int x = 0; x < 64; x++)
-		for (int y = 0; y < 64; y++)
-		{
-			int xorcolor = (x * 256 / TEX_WIDTH) ^ (y * 256 / TEX_HEIGHT);
-			//int xcolor = x * 256 / TEX_WIDTH;
-			int ycolor = y * 256 / TEX_HEIGHT;
-			int xycolor = y * 128 / TEX_HEIGHT + x * 128 / TEX_WIDTH;
-			texture[0 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 65536 * 254 * (x != y && x != TEX_WIDTH - y); //flat red texture with black cross
-			texture[1 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
-			texture[2 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
-			texture[3 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
-			texture[4 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 256 * xorcolor; //xor green
-			texture[5 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
-			texture[6 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 65536 * ycolor; //red gradient
-			texture[7 * TEX_WIDTH * TEX_HEIGHT + TEX_WIDTH * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-		}
-
 	// ========== SHADER COMPILATION ==========
 
 	// Compile
@@ -198,6 +179,10 @@ int main(int argc, char* argv[])
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("./pics/eagle.png", &width, &height, &nrChannels, 0);
+	stbi_image_free(data);
 
 
 	// ========== GAME LOOP ==========
